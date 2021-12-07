ff55c8ad82dc00a30cb0ffea0616f77ea67c0df8
erjabr <erjabr2@gmail.com>
2021-12-07 10:49:39 +0100

    Solved task 7

---
+++ Days/day7.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+with open("../Datasets/day7.txt") as file:
+    vals = []
+    for line in file:
+        vals = line.split(',')
+
+ints = list(map(int, list(vals)))
+dic = defaultdict(float)
+
+for i in range(max(ints)):
+    #print(i)
+    count = 0
+    steps = 0
+    for val in ints:
+        steps = abs(val-i)
+        count += ((steps*steps) + steps) / 2
+    dic[i] = count
+
+
+print(min(dic.values()))
